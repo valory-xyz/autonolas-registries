45c0d6570a61e21b18b4a26cc5ec9751407d23f3
Aleksandr Kuperman <aleksandr.kuperman@valory.xyz>
2022-07-25 11:18:04 +0100

    chore: updating npm packages

--- contracts/test/GnosisSafeABICreator.sol
+++ contracts/test/GnosisSafeABICreator.sol
@@ -3,5 +3,4 @@ pragma solidity ^0.8.15;
 
 // Getting ABIs for the Gnosis Safe master copy and proxy contracts
 import "@gnosis.pm/safe-contracts/contracts/GnosisSafe.sol";
-import "@gnosis.pm/safe-contracts/contracts/proxies/GnosisSafeProxy.sol";
 import "@gnosis.pm/safe-contracts/contracts/proxies/GnosisSafeProxyFactory.sol";
