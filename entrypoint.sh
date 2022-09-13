fd99edb1d6baeb760bf12f7c14a3534da7d76c37
Ardian <ardianabazi09@gmail.com>
2022-09-13 20:04:02 +0200

    fix: use `jq` args

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Using SERVICE_CONFIG_HASH = $SERVICE_CONFIG_HASH"
-jq '.serviceRegistry.configHashes = "$SERVICE_CONFIG_HASH"' snapshot.json > snapshot.json
+echo $(jq --arg SERVICE_CONFIG_HASH "$SERVICE_CONFIG_HASH" '.serviceRegistry.configHashes = [ $SERVICE_CONFIG_HASH ]' snapshot.json) > snapshot.json
 yarn hardhat node
