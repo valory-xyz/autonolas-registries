56829931d056fc2fd87f662ff523d27229094c8c
Aleksandr Kuperman <aleksandr.kuperman@valory.xyz>
2022-11-28 16:05:07 +0000

    chore: updating docker entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,6 @@
 if [[ -n "$SERVICE_CONFIG_HASH" ]]; then
   echo "Using SERVICE_CONFIG_HASH = $SERVICE_CONFIG_HASH"
   echo $(jq --arg SERVICE_CONFIG_HASH "$SERVICE_CONFIG_HASH" '.serviceRegistry.configHashes = [ $SERVICE_CONFIG_HASH ]' snapshot.json) > snapshot.json
-  yarn hardhat node
 fi
+
+yarn hardhat node
