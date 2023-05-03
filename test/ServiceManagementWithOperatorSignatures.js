633675233e8064729151f9fe7747f1b5a2f4a1c8
Aleksandr Kuperman <aleksandr.kuperman@valory.xyz>
2023-05-04 00:19:47 +0100

    test: full cycle service redeployment on-shot transaction

--- test/ServiceManagementWithOperatorSignatures.js
+++ test/ServiceManagementWithOperatorSignatures.js
@@ -17,6 +17,7 @@ describe("ServiceManagementWithOperatorSignatures", function () {
     let gnosisSafeSameAddressMultisig;
     let token;
     let operatorContract;
+    let timelock;
     let signers;
     let deployer;
     let operator;
@@ -85,6 +86,10 @@ describe("ServiceManagementWithOperatorSignatures", function () {
         const OperatorContract = await ethers.getContractFactory("MockOperatorContract");
         operatorContract = await OperatorContract.deploy();
         await operatorContract.deployed();
+
+        const Timelock = await ethers.getContractFactory("MockTimelock");
+        timelock = await Timelock.deploy();
+        await timelock.deployed();
 
         signers = await ethers.getSigners();
         deployer = signers[0];
@@ -348,7 +353,7 @@ describe("ServiceManagementWithOperatorSignatures", function () {
             // Getting a real multisig address
             const multisig = await ethers.getContractAt("GnosisSafe", proxyAddress);
 
-            // AT THIS POINT THE SERVICE IS CONSIDERED TO BE TRANSFERRED TO ANOTHER MULTISIG OWNER
+            // AT THIS POINT THE SERVICE IS CONSIDERED TO BE TRANSFERRED TO ANOTHER OWNER
 
             // Terminate a service after some time
             await serviceManager.connect(serviceOwner).terminate(serviceId);
@@ -534,6 +539,216 @@ describe("ServiceManagementWithOperatorSignatures", function () {
 
             // Redeploy the service updating the multisig with new owners and threshold
             await serviceManager.connect(serviceOwner).deploy(serviceId, gnosisSafeSameAddressMultisig.address, packedData);
+
+            // Check that the service is deployed
+            const service = await serviceRegistry.getService(serviceId);
+            expect(service.state).to.equal(4);
+        });
+
+        it("Redeploy service in one shot via the timelock contract", async function () {
+            const agentInstances = [signers[2], signers[3], signers[4], signers[5]];
+            const agentInstancesAddresses = [signers[2].address, signers[3].address, signers[4].address, signers[5].address];
+            const maxThreshold = 1;
+            const newMaxThreshold = 4;
+            const serviceOwner = signers[6];
+            let serviceOwnerAddress = serviceOwner.address;
+
+            // Mint tokens to the current service owner, timelock and the operator
+            const initSupply = "1" + "0".repeat(20);
+            await token.mint(operator.address, initSupply);
+            await token.mint(serviceOwnerAddress, initSupply);
+            await token.mint(serviceOwnerAddress, initSupply);
+            // Approve for the Service Registry Token Utility contract
+            await token.connect(operator).approve(serviceRegistryTokenUtility.address, initSupply);
+            await token.connect(serviceOwner).approve(serviceRegistryTokenUtility.address, initSupply);
+
+            // Create an agent
+            await agentRegistry.create(signers[0].address, defaultHash, [1]);
+
+            // Change Service Registry manager to the real one
+            await serviceRegistry.changeManager(serviceManager.address);
+            await serviceRegistryTokenUtility.changeManager(serviceManager.address);
+
+            // Create services and activate the agent instance registration
+            await serviceManager.connect(serviceOwner).create(serviceOwner.address, token.address, defaultHash, [1], [[1, regBond]], maxThreshold);
+
+            // Activate agent instance registration
+            await serviceManager.connect(serviceOwner).activateRegistration(serviceId, {value: 1});
+
+            /// Register agent instance
+            await serviceManager.connect(operator).registerAgents(serviceId, [agentInstances[0].address], [agentId], {value: 1});
+
+            // Whitelist both gnosis multisig implementations
+            await serviceRegistry.changeMultisigPermission(gnosisSafeMultisig.address, true);
+            await serviceRegistry.changeMultisigPermission(gnosisSafeSameAddressMultisig.address, true);
+
+            // Deploy the service and create a multisig and get its address
+            const safe = await serviceManager.connect(serviceOwner).deploy(serviceId, gnosisSafeMultisig.address, payload);
+            let result = await safe.wait();
+            const proxyAddress = result.events[0].address;
+            // Getting a real multisig address
+            const multisig = await ethers.getContractAt("GnosisSafe", proxyAddress);
+
+            // AT THIS POINT THE SERVICE IS CONSIDERED TO BE TRANSFERRED TO THE TIMELOCK
+            await serviceRegistry.connect(serviceOwner)["safeTransferFrom(address,address,uint256)"](serviceOwnerAddress,
+                timelock.address, serviceId);
+            serviceOwnerAddress = timelock.address;
+
+            // Send some ETH to the timelock address
+            await deployer.sendTransaction({to: serviceOwnerAddress, value: ethers.utils.parseEther("1000")});
+
+            // An agent instance gives the ownership rights to the new service owner or the timelock address
+            // In other words, swap the owner of the multisig to the service owner (agent instance to give up rights for the service owner)
+            // Since there was only one agent instance, the previous multisig owner address is the sentinel one defined by gnosis (0x1)
+            const safeContracts = require("@gnosis.pm/safe-contracts");
+            const sentinelOwners = "0x" + "0".repeat(39) + "1";
+            let nonce = await multisig.nonce();
+            let txHashData = await safeContracts.buildContractCall(multisig, "swapOwner",
+                [sentinelOwners, agentInstances[0].address, serviceOwnerAddress], nonce, 0, 0);
+            let signMessageData = await safeContracts.safeSignMessage(agentInstances[0], multisig, txHashData, 0);
+            await safeContracts.executeTx(multisig, txHashData, [signMessageData], 0);
+
+            let targets = new Array();
+            let values = new Array();
+            let payloads = new Array();
+
+            // Approve timelock tokens for the Service Registry Token Utility contract
+            targets.push(token.address);
+            values.push(0);
+            payloads.push(token.interface.encodeFunctionData("approve", [serviceRegistryTokenUtility.address, initSupply]));
+            //await token.connect(timelock).approve(serviceRegistryTokenUtility.address, initSupply);
+
+            // Terminate a service after some time
+            targets.push(serviceManager.address);
+            values.push(0);
+            payloads.push(serviceManager.interface.encodeFunctionData("terminate", [serviceId]));
+            //await serviceManager.connect(timelock).terminate(serviceId);
+
+            // Get the unbond function related data for the operator signed transaction
+            const unbondNonce = await serviceManager.getOperatorUnbondNonce(operator.address, serviceId);
+            const unbondTx = { operator: operator.address, serviceOwner: serviceOwnerAddress, serviceId: serviceId, nonce: unbondNonce };
+            const chainId = (await ethers.provider.getNetwork()).chainId;
+            const EIP712_UNBOND_TX_TYPE = {
+                // "Unbond(address operator,address serviceOwner,uint256 serviceId,uint256 nonce)"
+                Unbond: [
+                    { type: "address", name: "operator" },
+                    { type: "address", name: "serviceOwner" },
+                    { type: "uint256", name: "serviceId" },
+                    { type: "uint256", name: "nonce" },
+                ]
+            };
+
+            const managerName = await serviceManager.name();
+            const managerVersion = await serviceManager.version();
+            const EIP712_DOMAIN = { name: managerName, version: managerVersion, chainId: chainId, verifyingContract: serviceManager.address };
+            // Get the signature of an unbond transaction
+            let signatureBytes = await operator._signTypedData(EIP712_DOMAIN, EIP712_UNBOND_TX_TYPE, unbondTx);
+
+            // Unbond the agent instance in order to update the service using a pre-signed operator message
+            targets.push(serviceManager.address);
+            values.push(0);
+            payloads.push(serviceManager.interface.encodeFunctionData("unbondWithSignature", [operator.address,
+                serviceId, signatureBytes]));
+            //await serviceManager.connect(timelock).unbondWithSignature(operator.address, serviceId, ledgerSignatureBytes);
+
+            // Update a service
+            targets.push(serviceManager.address);
+            values.push(0);
+            payloads.push(serviceManager.interface.encodeFunctionData("update", [token.address, defaultHash, [1],
+                [[4, regBond]], newMaxThreshold, serviceId]));
+            //await serviceManager.connect(timelock).update(token.address, defaultHash, [1], [[4, regBond]], newMaxThreshold, serviceId);
+
+            // Activate agent instance registration
+            targets.push(serviceManager.address);
+            values.push(1);
+            payloads.push(serviceManager.interface.encodeFunctionData("activateRegistration", [serviceId]));
+            //await serviceManager.connect(timelock).activateRegistration(serviceId, {value: 1});
+
+            const agentIds = new Array(4).fill(agentId);
+            // Get the register agents function related data for the operator signed transaction
+            const registerAgentsNonce = await serviceManager.getOperatorRegisterAgentsNonce(operator.address, serviceId);
+            // Get the solidity counterpart of keccak256(abi.encode(agentInstances, agentIds))
+            const agentsData = ethers.utils.keccak256(ethers.utils.defaultAbiCoder.encode(["address[]", "uint32[]"],
+                [agentInstancesAddresses, agentIds]));
+            const registerAgentsTx = { operator: operator.address, serviceOwner: serviceOwnerAddress, serviceId: serviceId, agentsData: agentsData, nonce: registerAgentsNonce };
+            const EIP712_REGISTER_AGENTS_TX_TYPE = {
+                // "RegisterAgents(address operator,address serviceOwner,uint256 serviceId,bytes32 agentsData,uint256 nonce)"
+                RegisterAgents: [
+                    { type: "address", name: "operator" },
+                    { type: "address", name: "serviceOwner" },
+                    { type: "uint256", name: "serviceId" },
+                    { type: "bytes32", name: "agentsData" },
+                    { type: "uint256", name: "nonce" },
+                ]
+            };
+
+            // Get the signature for the register agents transaction
+            signatureBytes = await operator._signTypedData(EIP712_DOMAIN, EIP712_REGISTER_AGENTS_TX_TYPE, registerAgentsTx);
+
+            // Register agent instances via a signed operator transaction
+            targets.push(serviceManager.address);
+            values.push(agentInstances.length);
+            payloads.push(serviceManager.interface.encodeFunctionData("registerAgentsWithSignature", [operator.address,
+                serviceId, agentInstancesAddresses, agentIds, signatureBytes]));
+            //await serviceManager.connect(timelock).registerAgentsWithSignature(operator.address, serviceId,
+            //agentInstancesAddresses, agentIds, signatureBytes, {value: agentInstances.length});
+
+            // Change the existent multisig owners and threshold in a multisend transaction using the service owner access
+            let callData = [];
+            let txs = [];
+            nonce = await multisig.nonce();
+            // Add the addresses, but keep the threshold the same
+            for (let i = 0; i < agentInstances.length; i++) {
+                callData[i] = multisig.interface.encodeFunctionData("addOwnerWithThreshold", [agentInstances[i].address, 1]);
+                txs[i] = safeContracts.buildSafeTransaction({to: multisig.address, data: callData[i], nonce: 0});
+            }
+            // Remove the original multisig owner and change the threshold
+            // Note that the prevOwner is the very first added address as it corresponds to the reverse order of added addresses
+            // The order in the gnosis safe multisig is as follows: sentinelOwners => agentInstances[last].address => ... =>
+            // => newOwnerAddresses[0].address => serviceOwnerAddress
+            callData.push(multisig.interface.encodeFunctionData("removeOwner", [agentInstances[0].address, serviceOwnerAddress,
+                newMaxThreshold]));
+            txs.push(safeContracts.buildSafeTransaction({to: multisig.address, data: callData[callData.length - 1], nonce: 0}));
+
+            // Build a multisend transaction to be executed by the service multisig
+            const safeTx = safeContracts.buildMultiSendSafeTx(multiSend, txs, nonce);
+
+            // Create a message data from the multisend transaction
+            const messageHash = await multisig.getTransactionHash(safeTx.to, safeTx.value, safeTx.data,
+                safeTx.operation, safeTx.safeTxGas, safeTx.baseGas, safeTx.gasPrice, safeTx.gasToken,
+                safeTx.refundReceiver, nonce);
+
+            // Approve hash for the multisend transaction in the service multisig by the service owner multisig
+            targets.push(multisig.address);
+            values.push(0);
+            payloads.push(multisig.interface.encodeFunctionData("approveHash", [messageHash]));
+            //await multisig.connect(timelock).approveHash(messageHash);
+
+            // Get the signature line. Since the hash is approved, it's enough to base one on the service owner address
+            signatureBytes = "0x000000000000000000000000" + serviceOwnerAddress.slice(2) +
+                "0000000000000000000000000000000000000000000000000000000000000000" + "01";
+
+            // Form the multisend execTransaction call in the service multisig
+            const safeExecData = gnosisSafe.interface.encodeFunctionData("execTransaction", [safeTx.to, safeTx.value,
+                safeTx.data, safeTx.operation, safeTx.safeTxGas, safeTx.baseGas, safeTx.gasPrice, safeTx.gasToken,
+                safeTx.refundReceiver, signatureBytes]);
+
+            // Add the service multisig address on top of the multisig exec transaction data
+            const packedData = ethers.utils.solidityPack(["address", "bytes"], [multisig.address, safeExecData]);
+
+            // Redeploy the service updating the multisig with new owners and threshold
+            targets.push(serviceManager.address);
+            values.push(0);
+            payloads.push(serviceManager.interface.encodeFunctionData("deploy", [serviceId,
+                gnosisSafeSameAddressMultisig.address, packedData]));
+            //await serviceManager.connect(timelock).deploy(serviceId, gnosisSafeSameAddressMultisig.address, packedData);
+
+            //console.log(targets);
+            //console.log(values);
+            //console.log(payloads);
+
+            // Execute the timelock transaction that does the full cycle and redeploys the service
+            await timelock.executeBatch(targets, values, payloads);
 
             // Check that the service is deployed
             const service = await serviceRegistry.getService(serviceId);
