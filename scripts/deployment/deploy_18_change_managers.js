8ab50d2bb631fdf456dd402ed43b9b3e40447a1d
Aleksandr Kuperman <aleksandr.kuperman@valory.xyz>
2023-05-04 18:00:51 +0100

    chore: updating deployment scripts and docs

---
+++ scripts/deployment/deploy_18_change_managers.js
@@ -0,0 +1,48 @@
+/*global process*/
+
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+const { LedgerSigner } = require("@anders-t/ethers-ledger");
+
+async function main() {
+    const fs = require("fs");
+    const globalsFile = "globals.json";
+    const dataFromJSON = fs.readFileSync(globalsFile, "utf8");
+    let parsedData = JSON.parse(dataFromJSON);
+    const useLedger = parsedData.useLedger;
+    const derivationPath = parsedData.derivationPath;
+    const providerName = parsedData.providerName;
+    const serviceRegistryTokenUtilityAddress = parsedData.serviceRegistryTokenUtilityAddress;
+    const serviceManagerTokenAddress = parsedData.serviceManagerTokenAddress;
+    let EOA;
+
+    const provider = await ethers.providers.getDefaultProvider(providerName);
+    const signers = await ethers.getSigners();
+
+    if (useLedger) {
+        EOA = new LedgerSigner(provider, derivationPath);
+    } else {
+        EOA = signers[0];
+    }
+    // EOA address
+    const deployer = await EOA.getAddress();
+    console.log("EOA is:", deployer);
+
+    // Get the contract
+    const serviceRegistryTokenUtility = await ethers.getContractAt("ServiceRegistryTokenUtility", serviceRegistryTokenUtilityAddress);
+
+    // Transaction signing and execution
+    // 18. EOA to change the manager of ServiceRegistryTokenUtility to ServiceManagerToken calling `changeManager(ServiceManagerToken)`;
+    console.log("You are signing the following transaction: componentRegistry.connect(EOA).changeManager()");
+    let result = await serviceRegistryTokenUtility.connect(EOA).changeManager(serviceManagerTokenAddress);
+    // Transaction details
+    console.log("Contract address:", serviceRegistryTokenUtilityAddress);
+    console.log("Transaction:", result.hash);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
