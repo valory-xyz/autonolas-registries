c9beaf0c7f6b525bd7b06fb46df80c85d63dee78
Aleksandr Kuperman <aleksandr.kuperman@valory.xyz>
2023-08-14 16:26:14 +0100

    chore: adding a conditional lib script

---
+++ scripts/deployment/l2/libraries/deploy_01_conditional_lib.js
@@ -0,0 +1,85 @@
+/*global process*/
+
+const { ethers } = require("hardhat");
+const { LedgerSigner } = require("@anders-t/ethers-ledger");
+
+async function main() {
+    const fs = require("fs");
+    const globalsFile = "globals.json";
+    const dataFromJSON = fs.readFileSync(globalsFile, "utf8");
+    let parsedData = JSON.parse(dataFromJSON);
+    const useLedger = parsedData.useLedger;
+    const derivationPath = parsedData.derivationPath;
+    const providerName = parsedData.providerName;
+    const gasPriceInGwei = parsedData.gasPriceInGwei;
+    let EOA;
+
+    let networkURL;
+    if (providerName === "polygon") {
+        if (!process.env.ALCHEMY_API_KEY_MATIC) {
+            console.log("set ALCHEMY_API_KEY_MATIC env variable");
+        }
+        networkURL = "https://polygon-mainnet.g.alchemy.com/v2/" + process.env.ALCHEMY_API_KEY_MATIC;
+    } else if (providerName === "polygonMumbai") {
+        if (!process.env.ALCHEMY_API_KEY_MUMBAI) {
+            console.log("set ALCHEMY_API_KEY_MUMBAI env variable");
+            return;
+        }
+        networkURL = "https://polygon-mumbai.g.alchemy.com/v2/" + process.env.ALCHEMY_API_KEY_MUMBAI;
+    } else if (providerName === "gnosis") {
+        if (!process.env.GNOSIS_CHAIN_API_KEY) {
+            console.log("set GNOSIS_CHAIN_API_KEY env variable");
+            return;
+        }
+        networkURL = "https://rpc.gnosischain.com";
+    } else if (providerName === "chiado") {
+        networkURL = "https://rpc.chiadochain.net";
+    } else {
+        console.log("Unknown network provider", providerName);
+        return;
+    }
+
+    const provider = new ethers.providers.JsonRpcProvider(networkURL);
+    const signers = await ethers.getSigners();
+
+    if (useLedger) {
+        EOA = new LedgerSigner(provider, derivationPath);
+    } else {
+        EOA = signers[0];
+    }
+    // EOA address
+    const deployer = await EOA.getAddress();
+    console.log("EOA is:", deployer);
+
+    // Transaction signing and execution
+    console.log("1. EOA to deploy ConditionalLib");
+    const gasPrice = ethers.utils.parseUnits(gasPriceInGwei, "gwei");
+    const ConditionalLib = await ethers.getContractFactory("ConditionalLib");
+    console.log("You are signing the following transaction: ConditionalLib.connect(EOA).deploy()");
+    const conditionalLib = await ConditionalLib.connect(EOA).deploy({ gasPrice });
+    const result = await conditionalLib.deployed();
+
+    // Transaction details
+    console.log("Contract deployment: ServiceRegistryL2");
+    console.log("Contract address:", conditionalLib.address);
+    console.log("Transaction:", result.deployTransaction.hash);
+    // Wait half a minute for the transaction completion
+    await new Promise(r => setTimeout(r, 30000));
+
+    // Writing updated parameters back to the JSON file
+    parsedData.conditionalLibAddress = conditionalLib.address;
+    fs.writeFileSync(globalsFile, JSON.stringify(parsedData));
+
+    // Contract verification
+    if (parsedData.contractVerification) {
+        const execSync = require("child_process").execSync;
+        execSync("npx hardhat verify --network " + providerName + " " + conditionalLib.address, { encoding: "utf-8" });
+    }
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
