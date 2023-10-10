74ac3393fe55de4506cf190c28369f1f712ae388
AL <info@77ph.net>
2023-10-10 12:56:36 +0300

    test: Lib for nonce

---
+++ contracts/test/SafeNonceLib.sol
@@ -0,0 +1,22 @@
+pragma solidity =0.8.21;
+
+import {SafeStorage} from "./SafeStorage.sol";
+
+contract SafeNonceLib is SafeStorage {
+    event nonceUpdate(uint256 nonce);
+
+    function inceraseNonce(uint256 _nonce) external {
+        nonce += _nonce;
+        emit nonceUpdate(nonce);
+    }
+
+    function decreaseNonce(uint256 _nonce) external {         
+        if(nonce >= _nonce) {
+		    nonce += _nonce;
+	    } else {
+		    nonce = 0;
+	    }
+        emit nonceUpdate(nonce);
+    }
+
+}
