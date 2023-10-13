b9a9299e2a8aeafeb448e912cc5d1d1cba5ef96f
Aleksandr Kuperman <aleksandr.kuperman@valory.xyz>
2023-10-13 22:36:48 +0100

    refactor: just a temporary representation of how the multiple inheritance could be performed

--- contracts/staking/ServiceStakingMechAgentNativeToken.sol
+++ contracts/staking/ServiceStakingMechAgentNativeToken.sol
@@ -2,28 +2,14 @@
 pragma solidity ^0.8.21;
 
 import {ServiceStakingNativeToken} from "./ServiceStakingNativeToken.sol";
+import {MechAgentMod} from "./MechAgentMod.sol";
 
-// Multisig interface
-interface IMultisig {
-    /// @dev Gets the multisig nonce.
-    /// @return Multisig nonce.
-    function nonce() external view returns (uint256);
-}
-
-// AgentMech interface
-interface IAgentMech {
-    /// @dev Gets the requests count for a specific account.
-    /// @param account Account address.
-    /// @return requestsCount Requests count.
-    function getRequestsCount(address account) external view returns (uint256 requestsCount);
-}
-
-/// @title ServiceStakingMechAgentNativeToken - Smart contract for staking a service with the service interacting wiht
+/// @title ServiceStakingMechAgentNativeToken - Smart contract for staking a service with the service interacting with
 ///            AI agent mech and having a native network token as the deposit
 /// @author Aleksandr Kuperman - <aleksandr.kuperman@valory.xyz>
 /// @author Andrey Lebedev - <andrey.lebedev@valory.xyz>
 /// @author Mariapia Moscatiello - <mariapia.moscatiello@valory.xyz>
-contract ServiceStakingMechAgentNativeToken is ServiceStakingNativeToken {
+contract ServiceStakingMechAgentNativeToken is ServiceStakingNativeToken, MechAgentMod {
     // AI agent mech contract address.
     address public immutable agentMech;
 
@@ -41,13 +27,19 @@ contract ServiceStakingMechAgentNativeToken is ServiceStakingNativeToken {
         agentMech = _agentMech;
     }
 
+    function _getLivenessRatio() internal view override returns (uint256) {
+        return livenessRatio;
+    }
+
+    function _getAgentMech() internal view override returns (address) {
+        return agentMech;
+    }
+
     /// @dev Gets service multisig nonces.
     /// @param multisig Service multisig address.
     /// @return nonces Set of one or more service multisig nonces depending on implementation.
-    function _getMultisigNonces(address multisig) internal view override returns (uint256[] memory nonces) {
-        nonces = new uint256[](numNonces);
-        nonces[0] = IMultisig(multisig).nonce();
-        nonces[1] = IAgentMech(agentMech).getRequestsCount(multisig);
+    function _getMultisigNonces(address multisig) internal view override(ServiceStakingNativeToken, MechAgentMod) returns (uint256[] memory nonces) {
+        nonces = super._getMultisigNonces(multisig);
     }
 
     /// @dev Checks if the service multisig liveness ratio passes the defined liveness threshold.
@@ -59,15 +51,8 @@ contract ServiceStakingMechAgentNativeToken is ServiceStakingNativeToken {
         uint256[] memory curNonces,
         uint256[] memory lastNonces,
         uint256 ts
-    ) internal view override returns (bool ratioPass)
+    ) internal view override(ServiceStakingNativeToken, MechAgentMod) returns (bool ratioPass)
     {
-        uint256 diffNonces = curNonces[0] - lastNonces[0];
-        uint256 diffRequestsCounts = curNonces[1] - lastNonces[1];
-
-        // Sanity checks for requests counts difference to be at least half of the nonces difference
-        if (diffRequestsCounts <= diffNonces / 2) {
-            uint256 ratio = (diffRequestsCounts * 1e18) / ts;
-            ratioPass = (ratio >= livenessRatio / 2);
-        }
+        ratioPass = super._isRatioPass(curNonces, lastNonces, ts);
     }
 }
