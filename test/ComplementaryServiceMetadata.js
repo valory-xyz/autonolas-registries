8d100c29792672860b004e7bc52a3b34f047c5f7
Aleksandr Kuperman <aleksandr.kuperman@valory.xyz>
2025-05-22 13:23:43 +0100

    test: added tests

--- test/ComplementaryServiceMetadata.js
+++ test/ComplementaryServiceMetadata.js
@@ -38,7 +38,58 @@ describe("ComplementaryServiceMetadata", function () {
     });
 
     context("Change hash", function () {
+        const newHash = "0x1234567890123456789012345678901234567890123456789012345678901234";
 
+        it("Should allow service owner to change hash when service is not deployed", async function () {
+            const serviceId2 = serviceId + 100;
+            // Set the deployer to the service owner by default
+            await serviceRegistry.setServiceOwner(serviceId2, deployer.address);
+
+            // service Ids 101+ have mocked state as 0
+            await expect(complementaryServiceMetadata.changeHash(serviceId2, newHash))
+                .to.emit(complementaryServiceMetadata, "ComplementaryMetadataUpdated")
+                .withArgs(serviceId2, newHash);
+            
+            expect(await complementaryServiceMetadata.mapServiceHashes(serviceId2)).to.equal(newHash);
+        });
+
+        it("Should allow multisig to change hash when service is deployed", async function () {
+            // deployer is serviceId multisig
+            // ServiceId is in the deployed state
+            await expect(complementaryServiceMetadata.connect(deployer).changeHash(serviceId, newHash))
+                .to.emit(complementaryServiceMetadata, "ComplementaryMetadataUpdated")
+                .withArgs(serviceId, newHash);
+            
+            expect(await complementaryServiceMetadata.mapServiceHashes(serviceId)).to.equal(newHash);
+        });
+
+        it("Should not allow non-owner to change hash when service is not deployed", async function () {
+            const nonOwner = signers[1];
+
+            const serviceId2 = serviceId + 100;
+            // Set the deployer to the service owner by default
+            await serviceRegistry.setServiceOwner(serviceId2, deployer.address);
+
+            // service Ids 101+ have mocked state as 0
+
+            await expect(complementaryServiceMetadata.connect(nonOwner).changeHash(serviceId2, newHash))
+                .to.be.revertedWithCustomError(complementaryServiceMetadata, "UnauthorizedAccount")
+                .withArgs(nonOwner.address);
+        });
+
+        it("Should not allow non-multisig to change hash when service is deployed", async function () {
+            const nonMultisig = signers[1];
+            
+            await expect(complementaryServiceMetadata.connect(nonMultisig).changeHash(serviceId, newHash))
+                .to.be.revertedWithCustomError(complementaryServiceMetadata, "UnauthorizedAccount")
+                .withArgs(nonMultisig.address);
+        });
+
+        it("Should emit ComplementaryMetadataUpdated event with correct parameters", async function () {
+            await expect(complementaryServiceMetadata.changeHash(serviceId, newHash))
+                .to.emit(complementaryServiceMetadata, "ComplementaryMetadataUpdated")
+                .withArgs(serviceId, newHash);
+        });
     });
 
 });
