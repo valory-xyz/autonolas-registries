d0e1b780ecd68e872d5fcd9a653313efaec460ab
Aleksandr Kuperman <aleksandr.kuperman@valory.xyz>
2025-05-23 11:05:36 +0100

    chore: updating forge deployment script

--- scripts/deployment/deploy_22_safe_recovery_module.sh
+++ scripts/deployment/deploy_22_safe_recovery_module.sh
@@ -11,22 +11,20 @@ networkURL=$(jq -r '.networkURL' globals.json)
 multiSendCallOnlyAddress=$(jq -r '.multiSendCallOnlyAddress' globals.json)
 serviceRegistryAddress=$(jq -r '.serviceRegistryAddress' globals.json)
 
-ledgerCmd="--mnemonic-derivation-path $derivationPath"
-keyCmd="--private-key $PRIVATE_KEY"
-execCmd="forge create --broadcast --rpc-url $networkURL$ALCHEMY_API_KEY_MAINNET contracts/multsigs/RecoveryModule.sol:RecoveryModule --constructor-args $multiSendCallOnlyAddress $serviceRegistryAddress"
+execCmd="forge create --broadcast --rpc-url $networkURL$ALCHEMY_API_KEY_MAINNET contracts/multisigs/RecoveryModule.sol:RecoveryModule --constructor-args $multiSendCallOnlyAddress $serviceRegistryAddress"
 
 # Conditional logic (correct syntax)
 if [ "$useLedger" == "true" ]; then
-  execCmd="$execCmd $ledgerCmd"
+  execCmd="$execCmd --mnemonic-derivation-path $derivationPath"
 else
-  execCmd="$execCmd $keyCmd"
+  execCmd="$execCmd --private-key $PRIVATE_KEY"
 fi
 
 # Deploy the contract and capture the address
 recoveryModuleAddress=$($execCmd)
 
 # Write new deployed contract back into JSON
-jq --argjson recoveryModuleAddress "$recoveryModuleAddress" '.recoveryModuleAddress = $recoveryModuleAddress' globals.json > output.json
+echo "$(jq '. += {"recoveryModuleAddress":"'$recoveryModuleAddress'"}' globals.json)" > globals.json
 
 # Verify contract
 forge verify-contract \
