8583a4a353b755eb2933a2c97f341bd7f3a3bb17
Aleksandr Kuperman <aleksandr.kuperman@valory.xyz>
2025-11-20 13:33:57 +0000

    chore: configuration and static audit adjustment

--- scripts/audit_chains/audit_contracts_setup.js
+++ scripts/audit_chains/audit_contracts_setup.js
@@ -74,9 +74,16 @@ async function findContractInstance(provider, configContracts, contractName) {
     for (let i = 0; i < configContracts.length; i++) {
         if (configContracts[i]["name"] === contractName) {
             // Get the contract instance
-            const contractFromJSON = fs.readFileSync(configContracts[i]["artifact"], "utf8");
+            let contractFromJSON = fs.readFileSync(configContracts[i]["artifact"], "utf8");
+
+            // Additional step for the tokenomics proxy contract
+            if (contractName === "ServiceManagerProxy") {
+                // Get previous abi as it had ServiceManager implementation in it
+                contractFromJSON = fs.readFileSync(configContracts[i - 1]["artifact"], "utf8");
+            }
             const parsedFile = JSON.parse(contractFromJSON);
             const abi = parsedFile["abi"];
+
             const contractInstance = new ethers.Contract(configContracts[i]["address"], abi, provider);
             return contractInstance;
         }
@@ -174,7 +181,7 @@ async function checkServiceRegistry(chainId, provider, globalsInstance, configCo
     // Check manager
     const manager = await serviceRegistry.manager();
     // ServiceRegistryManagerToken
-    customExpect(manager, globalsInstance["serviceManagerTokenAddress"], log + ", function: manager()");
+    customExpect(manager, globalsInstance["serviceManagerAddress"], log + ", function: manager()");
 
     // Check drainer
     const drainer = await serviceRegistry.drainer();
@@ -205,8 +212,8 @@ async function checkServiceRegistry(chainId, provider, globalsInstance, configCo
     customExpect(res, true, log + ", function: mapMultisigs(safeMultisigWithRecoveryModule)");
 }
 
-// Check service manager: chain Id, provider, parsed globals, configuration contracts, contract name
-async function checkServiceManager(chainId, provider, globalsInstance, configContracts, contractName, log) {
+// Check service manager proxy: chain Id, provider, parsed globals, configuration contracts, contract name
+async function checkServiceManagerProxy(chainId, provider, globalsInstance, configContracts, contractName, log) {
     // Check the bytecode
     await checkBytecode(provider, configContracts, contractName, log);
 
@@ -217,17 +224,14 @@ async function checkServiceManager(chainId, provider, globalsInstance, configCon
     checkOwner(chainId, serviceManager, globalsInstance, log);
 
     log += ", address: " + serviceManager.address;
-    // Check service registry
-    const serviceRegistry = await serviceManager.serviceRegistry();
-    customExpect(serviceRegistry, globalsInstance["serviceRegistryAddress"], log + ", function: serviceRegistry()");
 
     // Check that the manager is not paused
     const paused = await serviceManager.paused();
     customExpect(paused, false, log + ", function: paused()");
 
-    // Version
-    const version = await serviceManager.version();
-    customExpect(version, "1.1.1", log + ", function: version()");
+    // Check service registry
+    const serviceRegistry = await serviceManager.serviceRegistry();
+    customExpect(serviceRegistry, globalsInstance["serviceRegistryAddress"], log + ", function: serviceRegistry()");
 
     // ServiceRegistryTokenUtility
     const serviceRegistryTokenUtility = await serviceManager.serviceRegistryTokenUtility();
@@ -254,7 +258,7 @@ async function checkServiceRegistryTokenUtility(chainId, provider, globalsInstan
     log += ", address: " + serviceRegistryTokenUtility.address;
     // Check manager
     const manager = await serviceRegistryTokenUtility.manager();
-    customExpect(manager, globalsInstance["serviceManagerTokenAddress"], log + ", function: manager()");
+    customExpect(manager, globalsInstance["serviceManagerAddress"], log + ", function: manager()");
 
     // Check drainer
     const drainer = await serviceRegistryTokenUtility.drainer();
@@ -540,9 +544,11 @@ async function main() {
                 await checkServiceRegistry(configs[i]["chainId"], providers[i], globals[i], configs[i]["contracts"], "ServiceRegistryL2", log);
             }
 
-            // Path for chains that operate with the ServiceManagerToken
-            log = initLog + ", contract: " + "ServiceManagerToken";
-            await checkServiceManager(configs[i]["chainId"], providers[i], globals[i], configs[i]["contracts"], "ServiceManagerToken", log);
+            // Skip celo for now
+            if (i != 6) {
+                log = initLog + ", contract: " + "ServiceManagerProxy";
+                await checkServiceManagerProxy(configs[i]["chainId"], providers[i], globals[i], configs[i]["contracts"], "ServiceManagerProxy", log);
+            }
 
             log = initLog + ", contract: " + "ServiceRegistryTokenUtility";
             await checkServiceRegistryTokenUtility(configs[i]["chainId"], providers[i], globals[i], configs[i]["contracts"], "ServiceRegistryTokenUtility", log);
