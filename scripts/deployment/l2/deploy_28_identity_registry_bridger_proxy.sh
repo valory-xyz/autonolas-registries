4934d4a6953d694f89c46553098b6d631ae93379
Aleksandr Kuperman <aleksandr.kuperman@valory.xyz>
2026-02-03 11:35:34 +0000

    chore: fix deployment script

--- scripts/deployment/l2/deploy_28_identity_registry_bridger_proxy.sh
+++ scripts/deployment/l2/deploy_28_identity_registry_bridger_proxy.sh
@@ -30,7 +30,8 @@ elif [ $chainId == 80002 ]; then
 fi
 
 identityRegistryBridgerAddress=$(jq -r '.identityRegistryBridgerAddress' $globals)
-proxyData=$(cast calldata "initialize()")
+agent8004BaseURI=$(jq -r '.agent8004BaseURI' $globals)
+proxyData=$(cast calldata "initialize(string memory)" "$agent8004BaseURI")
 
 contractName="IdentityRegistryBridgerProxy"
 contractPath="contracts/8004/$contractName.sol:$contractName"
